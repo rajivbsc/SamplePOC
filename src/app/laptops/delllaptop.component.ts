1396881ff7ed9e9e8712043eec98319e0fbc469e
rajivbsc <rajivbsc@gmail.com>
2020-07-28 21:05:33 +0530

    2 files added

---
+++ src/app/laptops/delllaptop.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core'
+
+@Component({
+selector:'dellLaptop',
+template:`<h1>dell Laptop</h1>`
+})
+
+export class dellLaptopComponent
+{}
---
+++ src/app/laptops/lenovolaptop.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core'
+
+@Component({
+selector:'lenovoLaptop',
+template:`<h1>Lenovo Laptop</h1>`
+})
+
+export class lenovoLaptopComponent
+{}
